87db1ef2514083d70fd1f10a649d11d86b7cac71
Savchuk Maksim <friiiman94@gmail.com>
2019-04-19 17:24:47 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import slick from './../../node_modules/slick-carousel/slick/slick.min.js';
+import svg4everybody from 'svg4everybody'
+
+svg4everybody();
+
+$(document).ready(function($){
+});
