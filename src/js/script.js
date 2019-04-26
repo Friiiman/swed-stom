34178fb0b6130bcb1a1dcdec6995266faeab355e
Savchuk Maksim <friiiman94@gmail.com>
2019-04-27 01:14:18 +0300

    services и doctors (img)

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,9 @@ import svg4everybody from 'svg4everybody'
 svg4everybody();
 
 $(document).ready(function($){
+
+  $('.doctors-card__button').click(function(){
+    $(this).siblings('.doctors-card__text-more').toggleClass('doctors-card__text-hide');
+  });
+
 });
