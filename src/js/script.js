bfb8cc42989b62a8a4d3d73d244fcc9a2eec245f
Savchuk Maksim <friiiman94@gmail.com>
2019-04-28 01:02:59 +0300

    ширина desktop

--- src/js/script.js
+++ src/js/script.js
@@ -10,19 +10,19 @@ $(document).ready(function($){
     $(this).siblings('.doctors-card__text-more').toggleClass('doctors-card__text--hide');
   });
 
+
   //***PARALLAX***
   $(window).scroll(function(){
     const st = $(this).scrollTop();
-    console.log(st);
 
     $('.promo__inner').css({
-      'transform' : 'translate(0%, -' + st /7 +'%'
+      'transform' : 'translate(0%, -' + st /14 +'%'
     });
 
     $('.about').css({
       'transform' : 'translate(0%, -' + st /7 +'%'
     });
-
   });
+
 
 });
