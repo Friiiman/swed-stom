084596efbb2f996eac5bfdda8e7d4b5f9ce707b2
Savchuk Maksim <friiiman94@gmail.com>
2019-07-09 02:06:01 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import slick from './../../node_modules/slick-carousel/slick/slick.min.js';
-import svg4everybody from 'svg4everybody'
+import svg4everybody from 'svg4everybody';
 
 svg4everybody();
 
